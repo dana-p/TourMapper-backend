93cae6787e3cb8baee3887277f9d052e3b87332f
Dana <daniela.prajea@gmail.com>
2018-12-17 21:21:16 -0500

    Removed default port

--- src/index.js
+++ src/index.js
@@ -197,6 +197,6 @@ app.post("/answer/:id", checkJwt, (req, res) => {
  |--------------------------------------
  */
 
-app.listen(8081, () => {
+app.listen(process.env.PORT || 8081, () => {
   console.log("Listening on port 8081");
 });
